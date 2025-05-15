513ec151745e8ac829101e6a91029291b18e5869
jack <90291236+jacknicklenson@users.noreply.github.com>
2025-05-15 14:37:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 
 int main(void) {
-  int image_width = 256;
-  int image_height = 256;
+  size_t image_width = 256;
+  size_t image_height = 256;
 
+  // prologue
   printf("P3\n%d %d\n255\n", image_width, image_height);
 
-  for (int j = 0; j < image_height; j++) {
-    for (int i = 0; i < image_width; i++) {
-      double r = (double)i / (image_width - 1);
-      double g = (double)j / (image_height - 1);
-      double b = 0.0;
+  for (size_t j = 0; j < image_height; j++) {
+    for (size_t i = 0; i < image_width; i++) {
+      vec3 col = {// map to 0.0 - 1.0 range
+                  (float)i / (image_width - 1), (float)j / (image_height - 1),
+                  0.0f};
 
-      int ir = (int)255.999 * r;
-      int ig = (int)255.999 * g;
-      int ib = (int)255.999 * b;
+      int ir = (int)(255.999 * col[0]);
+      int ig = (int)(255.999 * col[1]);
+      int ib = (int)(255.999 * col[2]);
 
       printf("%d %d %d\n", ir, ig, ib);
     }
