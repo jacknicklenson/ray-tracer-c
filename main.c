3f4678dab346ee29294b98536b9ff5d2fa387866
jack <90291236+jacknicklenson@users.noreply.github.com>
2025-05-15 17:17:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,81 @@
 #include "linmath.h"
+#include "ray.h"
 #include "stb_ds.h"
+#include <stddef.h>
 #include <stdio.h>
 
+void ray_col(vec3 out, struct ray *r) {
+  out[0] = 0.f;
+  out[1] = 0.f;
+  out[2] = 0.f;
+}
+
+void render(const vec3 pixel_color) {
+  int ir = (int)(255.999 * pixel_color[0]);
+  int ig = (int)(255.999 * pixel_color[1]);
+  int ib = (int)(255.999 * pixel_color[2]);
+  printf("%d %d %d\n", ir, ig, ib);
+}
+
 int main(void) {
-  size_t image_width = 256;
-  size_t image_height = 256;
+  size_t image_width = 400;
+  float aspect_ratio = 16.f / 9.f;
+  size_t image_height = (size_t)(image_width / aspect_ratio);
+
+  // viewport
+  size_t viewport_h = 2.0;
+  size_t viewport_w = viewport_h * ((float)image_width / image_height);
+
+  float focal_len = 1.0f;
+  vec3 camera_center = {0.f, 0.f, 0.f};
+
+  // Calculate the vectors across the horizontal and down the vertical viewport
+  // edges.
+  vec3 viewport_u = {viewport_w, 0.f, 0.f};
+  vec3 viewport_v = {0.f, -viewport_h, 0.f};
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  vec3 pixel_delta_u;
+  vec3_scale(pixel_delta_u, viewport_u, 1.f / (float)image_width);
+  vec3 pixel_delta_v;
+  vec3_scale(pixel_delta_v, viewport_v, 1.f / (float)image_height);
+
+  // Calculate the location of the upper left pixel.
+  vec3 viewport_upper_left;
+  {
+    vec3 out;
+    vec3_sub(viewport_upper_left, camera_center, (vec3){0.f, 0.f, focal_len});
+    vec3_scale(out, viewport_u, 0.5f);
+    vec3_sub(viewport_upper_left, viewport_upper_left, out);
+    vec3_scale(out, viewport_v, 0.5f);
+    vec3_sub(viewport_upper_left, viewport_upper_left, out);
+  }
+  vec3 pixel00_loc;
+  {
+    vec3 out;
+    vec3_add(out, pixel_delta_u, pixel_delta_v);
+    vec3_scale(pixel00_loc, out, 0.5f);
+    vec3_add(pixel00_loc, viewport_upper_left, pixel00_loc);
+  }
 
   // prologue
-  printf("P3\n%d %d\n255\n", image_width, image_height);
+  printf("P3\n%zu %zu\n255\n", image_width, image_height);
 
+  vec3 col, tmp, pixel_center, ray_dir;
+  struct ray r;
   for (size_t j = 0; j < image_height; j++) {
+    /* printf("[%%%f] Rendering Line: %zu / %zu\n", */
+    /*         (float)(j + 1) / (float)image_height, j + 1, image_height); */
     for (size_t i = 0; i < image_width; i++) {
-      vec3 col = {// map to 0.0 - 1.0 range
-                  (float)i / (image_width - 1), (float)j / (image_height - 1),
-                  0.0f};
-
-      int ir = (int)(255.999 * col[0]);
-      int ig = (int)(255.999 * col[1]);
-      int ib = (int)(255.999 * col[2]);
-
-      printf("%d %d %d\n", ir, ig, ib);
+      vec3_scale(tmp, pixel_delta_u, i);
+      vec3_add(pixel_center, tmp, pixel00_loc);
+      vec3_scale(tmp, pixel_delta_v, j);
+      vec3_add(pixel_center, tmp, pixel_center);
+      vec3_sub(ray_dir, pixel_center, camera_center);
+      vec4_dup(r.orig, camera_center);
+      vec4_dup(r.dir, ray_dir);
+      ray_col(col, &r);
+      render(col);
     }
   }
   return 0;
